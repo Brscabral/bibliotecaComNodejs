cc8377263ecceb1db7befc36e528c398e278e0ff
Brscabral <brscabral07@gmail.com>
2022-02-20 15:50:10 -0300

    implementando o promisses com async e await

--- index.js
+++ index.js
@@ -6,18 +6,41 @@ const fs = require('fs');//biblioteca nativa do nodejs.
 //[encoding]: geralmente utf-8
 //[callback]: a função que vamos usar para a resolução do nosso problema
 //detalhe importante da callback: ela vai receber um parametro de erro e sucesso
+
 function trataErro(erro){
-     throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'));
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'))
 }
-function pegaArquivo(caminhoDoArquivo){
-    const encoding='utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
-        if(erro){
-            trataErro(erro);
-        }else{
-            console.log(chalk.green(texto));
-        }
-    })
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
+      const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+      console.log(chalk.green(texto))
+    } catch(erro) {
+      trataErro(erro);
+    } finally {
+      console.log(chalk.yellow('operação concluída'));
+    }
+   }
+
+   pegaArquivo('./arquivos/texto1.md');
+
+//function pegaArquivo(caminhoDoArquivo){
+  //  const encoding='utf-8';
+  //  fs.promises //usamos para formas de códigos assicronas
+  //  .readFile(caminhoDoArquivo, encoding)
+  //  .then((texto)=>console.log(chalk.green(texto)))
+  //  .catch((erro)=>trataErro(erro))
+//}
+
+//function pegaArquivo(caminhoDoArquivo){
+  //  const encoding='utf-8';
+    //fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
+      //  if(erro){
+        //    trataErro(erro);
+        //}else{
+        //    console.log(chalk.green(texto));
+       // }
+   // })
    
-}
-pegaArquivo('./arquivos/texto1.md');
+//}
